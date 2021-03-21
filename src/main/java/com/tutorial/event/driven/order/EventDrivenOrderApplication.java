407d8e3fc476a5d411bcd206d43716036b2283c3
enrique <enrique.vilaplanatorres@gmail.com>
2021-03-21 14:07:15 +0100

    Initial commit

---
+++ src/main/java/com/tutorial/event/driven/order/EventDrivenOrderApplication.java
@@ -0,0 +1,12 @@
+package com.tutorial.event.driven.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenOrderApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(EventDrivenOrderApplication.class, args);
+  }
+}
